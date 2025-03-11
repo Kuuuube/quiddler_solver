5acf377af5e446c4b9a0a1a2b91497b5dddfa97c
kuuuube <hexagonisalie@gmail.com>
2025-03-11 11:51:51 -0400

    Comment out unused

--- src/double_letters.rs
+++ src/double_letters.rs
@@ -11,16 +11,16 @@ pub fn get_double_letter_symbol(letter: &str) -> &str {
     }
 }
 
-pub fn get_double_letter_from_symbol(letter: &str) -> &str {
-    return match letter {
-        "!" => "er",
-        "@" => "cl",
-        "#" => "in",
-        "$" => "th",
-        "%" => "qu",
-        _ => letter
-    }
-}
+// pub fn get_double_letter_from_symbol(letter: &str) -> &str {
+//     return match letter {
+//         "!" => "er",
+//         "@" => "cl",
+//         "#" => "in",
+//         "$" => "th",
+//         "%" => "qu",
+//         _ => letter
+//     }
+// }
 
 pub fn replace_all_double_letter_symbols(input_string: String) -> String {
     return input_string.replace("!", "er").replace("@", "cl").replace("#", "in").replace("$", "th").replace("%", "qu")
