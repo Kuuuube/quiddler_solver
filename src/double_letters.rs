9f5af97b2c4726da395aa9535f7e0c774bf0293c
kuuuube <hexagonisalie@gmail.com>
2025-03-11 11:27:52 -0400

    Convert back from double letter symbols when scoring

--- src/double_letters.rs
+++ src/double_letters.rs
@@ -11,13 +11,13 @@ pub fn get_double_letter_symbol(letter: &str) -> &str {
     }
 }
 
-pub fn get_double_letter_from_symbol(letter: &str) -> Option<String> {
+pub fn get_double_letter_from_symbol(letter: &str) -> &str {
     return match letter {
-        "!" => Some("er".to_string()),
-        "@" => Some("cl".to_string()),
-        "#" => Some("in".to_string()),
-        "$" => Some("th".to_string()),
-        "%" => Some("qu".to_string()),
-        _ => None
+        "!" => "er",
+        "@" => "cl",
+        "#" => "in",
+        "$" => "th",
+        "%" => "qu",
+        _ => letter
     }
 }
