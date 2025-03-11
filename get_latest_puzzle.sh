fa1361a94c82d0b68e7f91b1f2f547a67278d1e9
kuuuube <hexagonisalie@gmail.com>
2025-03-11 11:54:48 -0400

    Remove unnecessary echo

--- get_latest_puzzle.sh
+++ get_latest_puzzle.sh
@@ -1,5 +1,4 @@
 current_date=$(date +%Y-%m-%d_%H-%M-%S)
-echo $current_date
 mkdir puzzles_archive
 wget --output-document=./puzzles_archive/puzzle_$current_date https://www.setgame.com/quiddler/puzzle
 cp ./puzzles_archive/puzzle_$current_date puzzle
